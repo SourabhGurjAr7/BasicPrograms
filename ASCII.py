3ae40b765be2d51b74c365545a1ad033ee818d92
SourabhGurjAr7 <panwarsourabh33@gmail.com>
2022-12-17 14:21:59 +0530

    Some Basic programs

---
+++ ASCII.py
@@ -0,0 +1,9 @@
+'''def ASCII(ch):
+    return ord(ch)
+ch=input("Enter the string ")
+print("ASCII value is: ",ASCII(ch))
+'''
+
+ch=input("Enter the string :")
+for i in ch:
+    print("ASCII Value of",i,"is",ord(i))
