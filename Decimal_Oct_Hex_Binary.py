479c4d04a5dd8267783abb239eb9f3666d75751f
SourabhGurjAr7 <panwarsourabh33@gmail.com>
2022-12-17 14:08:13 +0530

    Some Basic programs

---
+++ Decimal_Oct_Hex_Binary.py
@@ -0,0 +1,10 @@
+def dec_to_Binary(dec1):
+    print("Decimal to Binary conversion is ",bin(dec1))
+def dec_to_Oct(dec1):
+    print("Decimal to Octal conversion is ",oct(dec1))
+def dec_to_Hex(dec1):
+        print("Decimal to Hexdecimal conversion is ", hex(dec1))
+dec1=int(input("Enter the Decimal Value to convert: "))
+dec_to_Binary(dec1)
+dec_to_Oct(dec1)
+dec_to_Hex(dec1)
