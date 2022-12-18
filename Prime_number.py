e500c268decdabaa0a268806b212f17295391ee9
SourabhGurjAr7 <panwarsourabh33@gmail.com>
2022-12-18 22:53:26 +0530

    Numbers Program

--- Prime_number.py
+++ Prime_number.py
@@ -9,14 +9,15 @@ else:
 
 #Printing all the prime numbers in the given interval
 low=10
-upr=50
+upr=59
+print("List of all the prime numbers in a given interval: ")
 for n in range(low,upr+1):
     if(n>1):
         for i in range(2,n):
             if(n%i==0):
               break
         else:
-            print(n)
+            print(n,end=" ")
 
 
 
