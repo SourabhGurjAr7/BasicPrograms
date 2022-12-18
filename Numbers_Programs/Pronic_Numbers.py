aebe57345030cbc79ed06a745b94a2e6c60909c5
SourabhGurjAr7 <panwarsourabh33@gmail.com>
2022-12-18 20:56:15 +0530

    Numbers Program

---
+++ Numbers_Programs/Pronic_Numbers.py
@@ -0,0 +1,21 @@
+#Check whether number is Pronic or not....12=3*4
+'''
+def Check_Pronic(n):
+    for i in range(1,n):
+        if(n==i*(i+1)):
+            print("Yes It is A Pronic Number")
+            break
+    else:
+        print("No it is not a Pronic number")
+n=int(input("Enter the Number: "))
+Check_Pronic(n)
+'''
+#print all the pronic numbers within a given range
+def Check_Pronic(n):
+    for i in range(1,n):
+        if(n==i*(i+1)):
+            return n
+print("List of all the Pronic Numbers in a given range: ")
+for i in range(0,500):
+    if(i==Check_Pronic(i)):
+        print(i,end=" ")
